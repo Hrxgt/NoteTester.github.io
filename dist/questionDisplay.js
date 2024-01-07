e4f96d71cdf6e94c0079ff74d20bfcbb6209aa15
Maxwell Nganyadi <maxwelltechmaster@outlook.com>
2024-01-06 21:13:26 -0700

    updating options blocks

--- dist/questionDisplay.js
+++ dist/questionDisplay.js
@@ -1,18 +1,25 @@
 "use strict";
 
+function updateQuestion(questionCount) {
+    // Display the first question
+    questionText[0].textContent = questions[questionCount].question;  // Blurred bg
+    questionText[1].textContent = questions[questionCount].question;  // Foreground
+
+    // Display the options
+    for (let i = 0; i < 4; i++) {
+        options[i].querySelector('.text-sm').textContent = questions[questionCount].options[i];
+
+    }    
+}
+
+
 let questions = JSON.parse(sessionStorage.getItem('questions'));
 let questionCount = 0;
 
 let questionText = document.getElementsByClassName('question');
 let options = document.getElementsByClassName('fgoption');
 
+let next_btn = document.getElementById('next_btn');
+next_btn.addEventListener('click', () => questionCount++);
 
-// Display the first question
-questionText[0].textContent = questions[questionCount].question;  // Blurred bg
-questionText[1].textContent = questions[questionCount].question;  // Foreground
 
-// Display the options
-for (let i = 0; i < 4; i++) {
-    options[i].querySelector('.text-sm').textContent = questions[questionCount].options[i];
-    
-}
